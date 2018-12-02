36e2cd9ec2515311843ec6281479fb88a820f266
Claire Furney <infuerno@fastmail.fm>
2018-12-02 16:48:06 +0000

    Add other modifed dotfiles from home directory

--- bootstrap-mine.sh
+++ bootstrap-mine.sh
@@ -1,3 +1,7 @@
 ln -s ~/Projects/GitHub/dotfiles/.aliases ~
 ln -s ~/Projects/GitHub/dotfiles/.bash_profile ~
 ln -s ~/Projects/GitHub/dotfiles/.bash_prompt ~
+ln -s ~/Projects/GitHub/dotfiles/.extra ~
+ln -s ~/Projects/GitHub/dotfiles/.gitconfig ~
+ln -s ~/Projects/GitHub/dotfiles/.vimrc ~
+
